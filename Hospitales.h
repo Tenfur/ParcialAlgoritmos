b8ab6088cf30e70ed990f74ba3e55356fc072bc0
Paolo <fercis2011@hotmail.com>
2020-04-25 19:37:48 -0500

    Primer commit

---
+++ Hospitales.h
@@ -0,0 +1,142 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <conio.h>
+#include <functional>
+
+using namespace std;
+template <class T1, class T2, class T3, class T4>
+class Hospital {
+	private:
+		Hospital *inicio;
+		Hospital *final;
+		Hospital *siguiente;
+		Hospital *anterior;
+		T1 nombre;
+		T2 departamento;
+		T3 respiradoresMecanicos;
+		T4 pacientes;
+		int contador = 0;
+	public:
+		Hospital(T1 nombre, T2 departamento, T3 respiradoresMecanicos, T4 pacientes) {
+			this->nombre = nombre;
+			this->departamento = departamento;
+			this->respiradoresMecanicos = respiradoresMecanicos;
+			this->pacientes = pacientes;
+			this->siguiente = NULL;
+		}
+		Hospital() {
+			this->inicio = NULL;
+			this->final = NULL;
+			this->anterior = NULL;
+		}
+		T3 getRespiradores() {
+			return respiradoresMecanicos;
+		}
+		T4 getPacientes() {
+			return pacientes;
+		}
+
+		int getContador() {
+			ifstream archivo;
+			string registro, nombre, departamento, numeroRespiradores, numeroPacientes, id;
+			int numero;
+			archivo.open("archivo.txt", ios::in);
+			while (getline(archivo, registro)) {
+				stringstream ss(registro);
+				getline(ss, nombre, ',');
+				getline(ss, departamento, ',');
+				getline(ss, numeroRespiradores, ',');
+				getline(ss, numeroPacientes, ',');
+				getline(ss, id);
+				numero = atoi(id.c_str());
+			}
+			archivo.close();
+			return numero;
+		}
+
+		void registrarHospital(T1 nombre, T2 departamento, T3 respiradoresMecanicos, T4 pacientes, int numeroPacientes) {
+			Hospital *nuevo = new Hospital(nombre, departamento, respiradoresMecanicos, pacientes);
+			contador = numeroPacientes;
+			ofstream archivo;
+			if (contador == 0) {
+				++contador;
+				inicio = final = nuevo;
+				archivo.open("archivo.txt", ios::out);
+				archivo << nombre << ',' << departamento << ',' << respiradoresMecanicos << ',' << pacientes << ',' << contador << endl;
+				cout << endl;
+				archivo.close();
+				
+			}
+			else{
+				++contador;
+				ifstream archivo2;
+				string registro, Snombre, Sdepartamento, SnumeroRespiradores, SnumeroPacientes;
+				int NnumeroRespiradores, NnumeroPacientes;
+				archivo2.open("archivo.txt", ios::in);
+				while (getline(archivo2, registro)) {
+					stringstream ss(registro);
+					getline(ss, Snombre, ',');
+					getline(ss, Sdepartamento, ',');
+					getline(ss, SnumeroRespiradores, ',');
+					getline(ss, SnumeroPacientes, ',');
+					NnumeroRespiradores = atoi(SnumeroRespiradores.c_str());
+					NnumeroPacientes = atoi(SnumeroPacientes.c_str());
+				}
+				archivo2.close();
+				Hospital *nuevo2 = new Hospital(Snombre, Sdepartamento, NnumeroRespiradores, NnumeroPacientes);
+				final = nuevo2;
+				final->siguiente = nuevo;
+				final = nuevo;
+				archivo.open("archivo.txt", ios::app);
+				archivo << nombre << ',' << departamento << ',' << respiradoresMecanicos << ',' << pacientes << ','  << contador << endl;
+				cout << endl;
+				archivo.close();
+			}
+		}
+		void mostrarHospitales() {
+			ifstream archivo;
+			archivo.open("archivo.txt", ios::in);
+			string registro, nombre, departamento, numeroRespiradores, numeroPacientes;
+			int cont = 0;
+			while (getline(archivo, registro)) {
+				stringstream ss(registro);
+				getline(ss, nombre, ',');
+				getline(ss, departamento, ',');
+				getline(ss, numeroRespiradores, ',');
+				getline(ss, numeroPacientes, ',');
+				cout << "Nombre: " << nombre << endl;
+				cout << "Departamento: " << departamento << endl;
+				cout << "Numero de respiradores: " << numeroRespiradores << endl;
+				cout << "Numero de pacientes: " << numeroPacientes << endl;
+				cout << endl << endl;
+				++cont;
+			}
+			cout << "Hospitales registrados: " << cont << endl;
+			_getch();
+			archivo.close();
+		}
+		void mostrarHospitalesSaturados(function<bool(Hospital<string,string,int,int>*)> condicion) {
+			ifstream archivo;
+			archivo.open("achivo.txt", ios::in);
+			string registro, nombre, departamento, numeroRespiradores, numeroPacientes;
+			int cont = 0, numeroPac;
+			while (getline(archivo, registro)) {
+				stringstream ss(registro);
+				getline(ss, nombre, ',');
+				getline(ss, departamento, ',');
+				getline(ss, numeroRespiradores, ',');
+				getline(ss, numeroPacientes, ',');
+				numeroPac = atoi(numeroPacientes.c_str());
+				if (condicion(numeroPac)) {
+					cout << nombre << endl;
+				}
+
+			}
+			archivo.close();
+		}
+
+
+};
---
+++ Menu.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <string.h>
+#include <functional>
+#include "Hospitales.h"
+
+using namespace std;
+
+int main() {
+	int opcion, opcion2, numeroDePacientes, numeroRespiradores;
+	string departamento, nombre;
+	Hospital <string, string, int, int> *obj = new Hospital<string, string, int, int>();
+	auto respiradoresSuficientes = [](Hospital <char, string, int, int> *obj) {
+		if (obj->getRespiradores() < 500) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+	do {
+		system("cls");
+		cout << "\tMinisterio de salud" << endl;
+		cout << "1) Registrar hospital " << endl;
+		cout << "2) Registrar paciente" << endl;
+		cout << "3) Mostrar informacion completa" << endl;
+		cout << "4) Ver datos en especifico" << endl;
+		cout << "5) Salir" << endl;
+		cout << "Ingrese opcion: ";
+		cin >> opcion;
+		if (opcion == 1) {
+			system("cls");
+			cin.ignore();
+			//char *nombre = new char[20];
+			int numero = obj->getContador();
+			cout << "Nombre: ";
+			//cin.getline(nombre, 20);
+			cin >> nombre;
+			cout << "Departamento: ";
+			cin >> departamento;
+			cout << "Numero de respiradores mecanicos: ";
+			cin >> numeroRespiradores;
+			cout << "Numero de pacientes: ";
+			cin >> numeroDePacientes;
+			obj->registrarHospital(nombre, departamento, numeroRespiradores, numeroDePacientes, numero);
+			cout << "Hospital registrado correctamente!" << endl;
+			_getch();
+		}
+		else if (opcion == 3) {
+			system("cls");
+			cout << "Hospitales registrados: " << endl;
+			obj->mostrarHospitales();
+			_getch();
+		}
+		else if (opcion == 4) {
+			system("cls");
+			cout << "\t Información detallada" << endl;
+			cout << "1) Hospitales saturados de pacientes" << endl;
+			cout << "2) Hospitales con insuficientes respiradores mecanicos" << endl;
+			cout << "Ingrese opcion: ";
+			cin >> opcion2;
+			if (opcion2 == 1) {
+				system("cls");
+				auto hospitalesSaturados = [](Hospital<string, string, int, int> *obj2) {
+					return (obj2->getPacientes() > 1000) ? true : false;
+				};
+				cout << "\t Hospitales saturados" << endl;
+				obj->mostrarHospitalesSaturados(hospitalesSaturados);
+				_getch();
+			}
+		}
+	} while (opcion !=5);
+
+
+
+	return 0;
+}
